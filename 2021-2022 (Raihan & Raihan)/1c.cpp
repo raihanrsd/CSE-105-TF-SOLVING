501e350490da2ea36f34adea787074bb650895e1
Mohammad Raihan Rashid <86001046+raihanrsd@users.noreply.github.com>
2023-10-25 15:56:32 +0600

    latest files uploaded

---
+++ 2021-2022 (Raihan & Raihan)/1c.cpp
@@ -0,0 +1,8 @@
+/*
+According to me getting the min and max from any heap at O(1) is not possible...
+We can either get the min or the max value from a MinHeap or MaxHeap respectively.
+
+Because in min heap the minimum value is always guaranteed to be at the top. 
+While for getting the max value we need to search the heap at O(n) complexity.
+
+*/
